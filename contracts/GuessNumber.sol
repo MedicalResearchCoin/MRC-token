6c02742cf25e78a8094dd451d8b2b3f06fd3af07
Daniel Kaminski de Souza <daniel.souza@cceg.org.uk>
2017-05-27 08:59:56 -0300

    added .gitignore file

--- contracts/GuessNumber.sol
+++ contracts/GuessNumber.sol
@@ -1,22 +1,22 @@
 pragma solidity ^0.4.2;
 
 contract GuessNumber{
-    
+
     uint8 number;
-    
+
     event Deposit(address from, uint value);
     event SetNumber();
 
     function() payable{
         if (msg.value > 0)
-            Deposit(msg.sender, msg.value);
+        Deposit(msg.sender, msg.value);
     }
-    
+
     function setNumber(uint8 givenNumber) {
         number = givenNumber;
         SetNumber();
     }
-    
+
     function guessNumber(uint8 givenNumber) constant returns (bool) {
         return (givenNumber == number);
     }
